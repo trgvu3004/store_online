b0a8af9f361f0a657557df43a0e47e49551a5b3b
Trường Vũ <truongvu6021@gmail.com>
2021-11-04 17:17:02 +0700

    feat: launch screen

--- app/src/main/java/com/example/store_online/SplashActivity.java
+++ app/src/main/java/com/example/store_online/SplashActivity.java
@@ -1,14 +1,52 @@
 package com.example.store_online;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.example.store_online.authentication.SignInActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SplashActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseUser mUser;
+    private Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        //hide action bar
+        getSupportActionBar().hide();
+        //initialization
+        mAuth = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        handler = new Handler();
+        //delay pop up screen
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //Checked sign in
+                autoSignIn();
+            }
+        }, 3000);
+
+    }
+    private void autoSignIn() {
+        if (mUser == null) {
+            //redirect to sign in screen
+            startActivity(new Intent(SplashActivity.this, SignInActivity.class));
+        } else {
+            //redirect to main screen
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+        }
+        //close splash screen
+        finish();
     }
 }
