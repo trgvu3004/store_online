5bb0ffa8e5f933561a962b9ca8a28feba15528cc
Trường Vũ <truongvu6021@gmail.com>
2021-11-04 21:27:30 +0700

    feat(authentication): Sign in, Sign up

--- app/src/main/java/com/example/store_online/authentication/SignInActivity.java
+++ app/src/main/java/com/example/store_online/authentication/SignInActivity.java
@@ -2,15 +2,48 @@ package com.example.store_online.authentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.example.store_online.MainActivity;
 import com.example.store_online.R;
 
 public class SignInActivity extends AppCompatActivity {
+    private TextView txtSignUp;
+    private  Button btnSignIn;
+    private EditText edtPhone,edtPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        mapping();
+        setEvent();
+    }
+    private void setEvent() {
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignInActivity.this, MainActivity.class));
+            }
+        });
+        txtSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SignInActivity.this,SignUpActivity.class));
+            }
+        });
+    }
+
+    private void mapping() {
+        txtSignUp=findViewById(R.id.txt_sign_up);
+        edtPhone=findViewById(R.id.edt_sign_in_phone);
+        edtPassword=findViewById(R.id.edt_sign_in_password);
+        btnSignIn=findViewById(R.id.btn_sign_in);
     }
 }
---
+++ app/src/main/java/com/example/store_online/authentication/SignUpActivity.java
@@ -0,0 +1,23 @@
+package com.example.store_online.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.store_online.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+}
