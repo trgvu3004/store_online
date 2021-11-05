18c9b1a274173108478e77694eaafb0d58bb303d
Trường Vũ <truongvu6021@gmail.com>
2021-11-05 16:38:52 +0700

    feat: Added bottom navigation

--- app/src/main/java/com/example/store_online/MainActivity.java
+++ app/src/main/java/com/example/store_online/MainActivity.java
@@ -1,14 +1,93 @@
 package com.example.store_online;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.example.store_online.adapter.ViewPagerAdapter;
+import com.example.store_online.fragment.AccountFragment;
+import com.example.store_online.fragment.CategoryFragment;
+import com.example.store_online.fragment.HomePageFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.google.android.material.navigation.NavigationView;
+
+import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+    private ViewPagerAdapter adapter;
+    private ViewPager2 viewPager2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapping();
+        setEvent();
+    }
+
+    private void setEvent() {
+        adapter=new ViewPagerAdapter(this);
+        //set adapter for viewpage2
+        viewPager2.setAdapter(adapter);
+        //set Event change page
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                switch (position){
+                    case 0:
+                        bottomNavigationView.getMenu().findItem(R.id.menu_home_page).setChecked(true);
+                        break;
+                    case 1:
+                        bottomNavigationView.getMenu().findItem(R.id.menu_category).setChecked(true);
+                        break;
+                    case 2:
+                        bottomNavigationView.getMenu().findItem(R.id.menu_account).setChecked(true);
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+                super.onPageScrollStateChanged(state);
+            }
+        });
+        //Selected item bottom navigation
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.menu_home_page:
+                       viewPager2.setCurrentItem(0);
+                        break;
+                    case R.id.menu_category:
+                        viewPager2.setCurrentItem(1);
+                        break;
+                    case R.id.menu_account:
+                        viewPager2.setCurrentItem(2);
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    private void mapping() {
+        viewPager2=findViewById(R.id.view_page_main);
+        bottomNavigationView=findViewById(R.id.navigation_bottom);
     }
 }
