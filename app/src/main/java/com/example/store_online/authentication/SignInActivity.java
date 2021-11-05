e2f8129af9e30bb7c6ac5822da63a89226095245
Trường Vũ <truongvu6021@gmail.com>
2021-11-05 08:34:22 +0700

    refactor(Sign in): sign in with email and password

--- app/src/main/java/com/example/store_online/authentication/SignInActivity.java
+++ app/src/main/java/com/example/store_online/authentication/SignInActivity.java
@@ -1,5 +1,6 @@
 package com.example.store_online.authentication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,39 +12,68 @@ import android.widget.TextView;
 
 import com.example.store_online.MainActivity;
 import com.example.store_online.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignInActivity extends AppCompatActivity {
     private TextView txtSignUp;
-    private  Button btnSignIn;
-    private EditText edtEmail,edtPassword;
+    private Button btnSignIn;
+    private EditText edtEmail, edtPassword;
+    private FirebaseAuth mAuth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+        //init Firebase
+        mAuth = FirebaseAuth.getInstance();
         mapping();
         setEvent();
     }
+
     private void setEvent() {
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(SignInActivity.this, MainActivity.class));
+                signUp();
             }
         });
         txtSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(SignInActivity.this,SignUpActivity.class));
+                startActivity(new Intent(SignInActivity.this, SignUpActivity.class));
             }
         });
     }
 
+    private void signUp() {
+        String email = edtEmail.getText().toString().trim();
+        String password = edtPassword.getText().toString().trim();
+        if (email.isEmpty()) {
+            edtEmail.setError("Email is not empty!");
+        } else if (password.isEmpty()) {
+            edtPassword.setError("Email is not empty!");
+        } else {
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        startActivity(new Intent(SignInActivity.this, MainActivity.class));
+                    } else {
+
+                    }
+                }
+            });
+        }
+    }
+
     private void mapping() {
-        txtSignUp=findViewById(R.id.txt_sign_up);
-        edtEmail=findViewById(R.id.edt_sign_in_email);
-        edtPassword=findViewById(R.id.edt_sign_in_password);
-        btnSignIn=findViewById(R.id.btn_sign_in);
+        txtSignUp = findViewById(R.id.txt_sign_up);
+        edtEmail = findViewById(R.id.edt_sign_in_email);
+        edtPassword = findViewById(R.id.edt_sign_in_password);
+        btnSignIn = findViewById(R.id.btn_sign_in);
     }
 }
