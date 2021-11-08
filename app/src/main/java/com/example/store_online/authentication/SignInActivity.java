7e3fea25f98d34cfe309e5a59ecf2857fb5a4202
Trường Vũ <truongvu6021@gmail.com>
2021-11-08 09:31:41 +0700

    feat(Authentication): Sign in with Google

--- app/src/main/java/com/example/store_online/authentication/SignInActivity.java
+++ app/src/main/java/com/example/store_online/authentication/SignInActivity.java
@@ -9,15 +9,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.store_online.MainActivity;
 import com.example.store_online.R;
 import com.example.store_online.dialog.ErrorDialog;
 import com.example.store_online.dialog.LoadingDialog;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class SignInActivity extends AppCompatActivity {
     private TextView txtSignUp;
@@ -26,7 +35,10 @@ public class SignInActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private LoadingDialog loadingDialog;
     private ErrorDialog errorDialog;
+    private FloatingActionButton btnGoogle, btnFacebook;
     private String MESSAGE_SIGN_IN_ERROR = "Incorrect account or password!";
+    private GoogleSignInClient mGoogleSignInClient;
+    private int RC_SIGN_IN = 1;
 
 
     @Override
@@ -38,6 +50,9 @@ public class SignInActivity extends AppCompatActivity {
         //init dialog
         loadingDialog = new LoadingDialog(SignInActivity.this);
         errorDialog = new ErrorDialog(this);
+        //
+        request();
+        //
         mapping();
         setEvent();
     }
@@ -53,6 +68,18 @@ public class SignInActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(SignInActivity.this, SignUpActivity.class));
+            }
+        });
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signInwithGoogle();
+            }
+        });
+        btnFacebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
@@ -74,6 +101,7 @@ public class SignInActivity extends AppCompatActivity {
                         startActivity(new Intent(SignInActivity.this, MainActivity.class));
                         finishAffinity();
                     } else {
+                        loadingDialog.endLoadingDialog();
                         errorDialog.startErrorDialog(MESSAGE_SIGN_IN_ERROR);
                     }
                 }
@@ -81,10 +109,64 @@ public class SignInActivity extends AppCompatActivity {
         }
     }
 
+    private void request() {
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
+
+    private void signInwithGoogle() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount idToken) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            startActivity(new Intent(SignInActivity.this, MainActivity.class));
+                            finishAffinity();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(getApplicationContext(), "Sign in failed!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+
     private void mapping() {
         txtSignUp = findViewById(R.id.txt_sign_up);
         edtEmail = findViewById(R.id.edt_sign_in_email);
         edtPassword = findViewById(R.id.edt_sign_in_password);
         btnSignIn = findViewById(R.id.btn_sign_in);
+        btnGoogle = findViewById(R.id.btn_Google);
+        btnFacebook = findViewById(R.id.btn_Facebook);
     }
 }
