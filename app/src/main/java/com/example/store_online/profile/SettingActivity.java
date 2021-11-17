de8829493fe6a854f111b36062191fd86af55169
Trường Vũ <truongvu6021@gmail.com>
2021-11-17 08:44:22 +0700

    feat: added account information

--- app/src/main/java/com/example/store_online/profile/SettingActivity.java
+++ app/src/main/java/com/example/store_online/profile/SettingActivity.java
@@ -1,16 +1,14 @@
 package com.example.store_online.profile;
 
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
 import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-
-
+import android.widget.TextView;
 
 import com.example.store_online.R;
 import com.example.store_online.authentication.SignInActivity;
@@ -21,7 +19,7 @@ public class SettingActivity extends AppCompatActivity {
     private Button btnSignOut;
     private FirebaseAuth mAuth;
     private LoadingDialog loadingDialog;
-    private Toolbar toolbar;
+    private TextView txtUserInformation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,25 +28,17 @@ public class SettingActivity extends AppCompatActivity {
         //mapping view
         mapping();
         //init firebase
-        mAuth=FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
         //int lodding dialog
         loadingDialog = new LoadingDialog(this);
         //set ToolBar
-        setSupportActionBar(toolbar);
         getSupportActionBar().setTitle(R.string.setting_title);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_baseline_arrow_back_24);
         //set Event Listening
         setEvent();
     }
 
     private void setEvent() {
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-            }
-        });
         btnSignOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -59,10 +49,22 @@ public class SettingActivity extends AppCompatActivity {
                 finishAffinity();
             }
         });
+        txtUserInformation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SettingActivity.this, AccountInformationActivity.class));
+            }
+        });
     }
 
     private void mapping() {
         btnSignOut = findViewById(R.id.btn_sign_out_setting);
-        toolbar = findViewById(R.id.tbSetting);
+        txtUserInformation = findViewById(R.id.txtInforUser);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        onBackPressed();
+        return true;
     }
 }
